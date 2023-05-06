a8c564b250fd157c7cb997ca5ce23ac39a101347
Abdelmoneim Reyad <79276407+Men3m10@users.noreply.github.com>
2023-05-06 14:08:14 +0300

    Update validator.js

--- middlewares/validator.js
+++ middlewares/validator.js
@@ -4,11 +4,7 @@ const validator = (req, res, next) => {
   // Finds the validation errors in this request and wraps them in an object with handy functions
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    const messages = errors
-      .array()
-      .map((error) => ({ param: error.param, msg: error.msg }));
-
-    return res.status(400).json({ errors: messages });
+    return res.status(400).json({ errors: errors.array() });
   }
   next();
 };
