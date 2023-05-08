9d7fd1f7f7083afe0631d0b7e224a54bb718c2e3
Abdelmoneim Reyad <79276407+Men3m10@users.noreply.github.com>
2023-05-08 15:10:04 +0300

    Update authValidation.js

--- utils/validation/authValidation.js
+++ utils/validation/authValidation.js
@@ -8,32 +8,28 @@ const signUpValidation = (
   confirmPassword
 ) => {
   if (name.length < 3 || name.length == 0) {
-    return res.status(400).json({ message: "too short Password" });
+    return res.json({ message: "too short Password" });
   }
   if (email.length == 0) {
-    return res.status(400).json({ message: "email required" });
+    return res.json({ message: "email required" });
   }
   if (password.length < 6 || password.length == 0) {
-    return res.status(400).json({ message: "too short Password" });
+    return res.json({ message: "too short Password" });
   }
   if (password != req.body.confirmPassword) {
-    return res
-      .status(400)
-      .json({ message: "password confirmation is inCorrect" });
+    return res.json({ message: "password confirmation is inCorrect" });
   }
   if (ssid.length > 14 || ssid.length < 14 || ssid.length == 0) {
-    return res.status(400).json({ message: "national id must be 14 number" });
+    return res.json({ message: "national id must be 14 number" });
   }
   if (confirmPassword.length < 6 || confirmPassword.length == 0) {
-    return res.status(400).json({ message: "confirm password is required" });
+    return res.json({ message: "confirm password is required" });
   }
 };
 
 const logInValidation = (res, password, ssid) => {
   if (password.length < 6 || ssid.length > 14 || ssid.length < 14) {
-    return res
-      .status(400)
-      .json({ message: "check national id or password you enterd" });
+    return res.json({ message: "check national id or password you enterd" });
   }
 };
 module.exports = { signUpValidation, logInValidation };
