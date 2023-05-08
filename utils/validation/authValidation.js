d9d2aed00a0d4f3157d461a9caa1f9e748396861
Abdelmoneim Reyad <79276407+Men3m10@users.noreply.github.com>
2023-05-08 10:10:36 +0300

    Update authValidation.js

--- utils/validation/authValidation.js
+++ utils/validation/authValidation.js
@@ -1,74 +1,39 @@
-const { check } = require("express-validator");
-const slugify = require("slugify");
+const signUpValidation = (
+  req,
+  res,
+  name,
+  email,
+  password,
+  ssid,
+  confirmPassword
+) => {
+  if (name.length < 3 || name.length == 0) {
+    return res.status(400).json({ message: "too short Password" });
+  }
+  if (email.length == 0) {
+    return res.status(400).json({ message: "email required" });
+  }
+  if (password.length < 6 || password.length == 0) {
+    return res.status(400).json({ message: "too short Password" });
+  }
+  if (password != req.body.confirmPassword) {
+    return res
+      .status(400)
+      .json({ message: "password confirmation is inCorrect" });
+  }
+  if (ssid.length > 14 || ssid.length < 14 || ssid.length == 0) {
+    return res.status(400).json({ message: "national id must be 14 number" });
+  }
+  if (confirmPassword.lengthlength < 6 || confirmPassword.length == 0) {
+    return res.status(400).json({ message: "confirm password is required" });
+  }
+};
 
-const validation = require("../../middlewares/validator");
-
-const User = require("../../models/userModel");
-
-exports.signupValidator = [
-  check("name")
-    .notEmpty()
-    .withMessage("User name is required ")
-    .trim()
-    .isLength({ min: 3 })
-    .withMessage("too short User name ")
-    .custom((val, { req }) => {
-      req.body.slug = slugify(val);
-      return true;
-    }),
-
-  check("email")
-    .notEmpty()
-    .withMessage("Email is required")
-    .isEmail()
-    .withMessage("inValid Email")
-    .custom((val) =>
-      User.findOne({ email: val }).then((user) => {
-        if (user) {
-          return Promise.reject(new Error(`email already exist`));
-        }
-      })
-    ),
-
-  check("password")
-    .notEmpty()
-    .withMessage("Password is required")
-    .isLength({ min: 6 })
-    .withMessage("too short Password")
-    .custom((password, { req }) => {
-      // eslint-disable-next-line eqeqeq
-      if (password != req.body.confirmPassword) {
-        throw new Error("password confirmation is inCorrect");
-      }
-      return true;
-    }),
-
-  check("confirmPassword")
-    .notEmpty()
-    .withMessage("confirm password is required"),
-  check("ssid")
-    .notEmpty()
-    .withMessage("national id is required")
-    .isLength({ min: 14 })
-    .withMessage("national id must be 14 num")
-    .isLength({ max: 14 })
-    .withMessage("national id must be 14 num"),
-  validation,
-];
-
-exports.loginValidator = [
-  check("ssid")
-    .notEmpty()
-    .withMessage("national id is required")
-    .isLength({ min: 14 })
-    .withMessage("national id must be 14 num")
-    .isLength({ max: 14 })
-    .withMessage("national id must be 14 num"),
-
-  check("password")
-    .notEmpty()
-    .withMessage("Password is required")
-    .isLength({ min: 6 })
-    .withMessage("too short Password"),
-  validation,
-];
+const logInValidation = (res, password, ssid) => {
+  if (password.length < 6 || ssid.length > 14 || ssid.length < 14) {
+    return res
+      .status(400)
+      .json({ message: "check national id or password you enterd" });
+  }
+};
+module.exports = { signUpValidation, logInValidation };
