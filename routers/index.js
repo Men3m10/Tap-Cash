f7029b4d37c6f73e6b6fa9fa3c83223421d8a4df
Abdelmoneim Reyad <79276407+Men3m10@users.noreply.github.com>
2023-05-08 15:04:49 +0300

    Update index.js

--- routers/index.js
+++ routers/index.js
@@ -1,13 +1,13 @@
 const userRoute = require("./userRoutes");
 const authRoute = require("./authRoutes");
-const requestRoute = require("./requestRoutes");
+const visaRoute = require("./visaRoutes");
 const transactionRoute = require("./transactionRoutes");
 const walletRoute = require("./walletRoutes");
 
 const Routes = (app) => {
   app.use("/api/v1/users", userRoute);
   app.use("/api/v1/auth", authRoute);
-  app.use("/api/v1/request", requestRoute);
+  app.use("/api/v1/visa", visaRoute);
   app.use("/api/v1/transaction", transactionRoute);
   app.use("/api/v1/wallet", walletRoute);
 };
