ba0de713ab54b7b456ef64d8b606d23825f7545c
Abdelmoneim Reyad <79276407+Men3m10@users.noreply.github.com>
2023-05-08 19:25:21 +0300

    Update visaRoutes.js

--- routers/visaRoutes.js
+++ routers/visaRoutes.js
@@ -10,14 +10,10 @@ const {
 } = require("../controllers/visaController");
 
 //for all routes under
-Router.use(auth.Protect);
+// Router.use(auth.Protect);
 Router.use(auth.allowedTo("parent", "child"));
 Router.post("/create", createVisa);
 Router.post("/pay", checkExpiredVisa, payByVisa);
 
-// Router.get("/", getRequests);
-// Router.get("/:id", getRequestValidator, getRequestById);
-// Router.put("/:id", updateRequestById);
-// Router.delete("/:id", deleteRequestValidator, deleteRequestById);
 
 module.exports = Router;
